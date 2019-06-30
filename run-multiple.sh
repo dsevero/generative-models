83f2e5ea08b0830b5af129f03063ee70489ea40e
Daniel Severo <daniel.severo@3778.care>
2019-06-30 19:50:28 -0300

    Add kaggle api

--- run-multiple.sh
+++ run-multiple.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for d in 0.0 0.25 0.50 0.75 0.90; do
-	for b in 16 32 64 128 256 512 1024 2048 4096; do
-		poetry run mlflow run projects/mnist -P batch_size=$b -P dropout_rate=$d -P epochs=20 -P architecture=DenseCNN
+	for b in 512 1024 2048 4096; do
+		poetry run mlflow run experiments/MNIST/DenseCNN --experiment-name MNIST -P batch_size=$b -P dropout_rate=$d -P epochs=100 -P architecture=DenseCNN
 	done
 done
